d9f954e92c69201519428e96c22ccbe8293c649b
AliDlt <alidlt1@yahoo.com>
2024-06-25 17:32:50 +0330

    update slider

--- templates/Slider/main-slider.php
+++ templates/Slider/main-slider.php
@@ -5,8 +5,8 @@
             <p>در نیتروپراپ چالش ها محدودیت زمانی ندارند و بدون نگرانی از بابت محدودیت های ۳۰ و ۶۰ روزه برای پاس کردن
                 چالش ها ، می توانید مراحل چالش خود را طی کنید.</p>
             <div class="btn-sec-slider-01">
-                <button class="slider-login-btn">ورود / عضویت</button>
-                <button class="slider-preview-dashboard-btn">پیش نمایش داشبورد</button>
+                <button onclick="window.location.href='https://nitroprop.live/panel/" class="slider-login-btn">خرید چالش</button>
+                <button class="slider-preview-dashboard-btn">ورود به داشبورد</button>
             </div>
         </div>
         <div class="right-slider-01">
@@ -18,21 +18,8 @@
             <h2>تقسیم سود ۵ درصدی از مجموع سود چالش ها</h2>
             <p>با اولین برداشت سود در حساب ریل علاوه بر ریفاند هزینه چالش، ۵ درصد از مجموع سود دو مرحله  چالش نیز برای شما واریز می شود.</p>
             <div class="btn-sec-slider-01">
-                <button class="slider-login-btn">ورود / عضویت</button>
-                <button class="slider-preview-dashboard-btn">پیش نمایش داشبورد</button>
-            </div>
-        </div>
-        <div class="right-slider-01">
-            <img src="<?php echo NCP_PLUGIN_INCLUDES_URL . 'front-assets/img/slider/slider-02.png' ?>" alt="">
-        </div>
-    </div>
-    <div class="mySlides fade slider-01" style="display: none">
-        <div class="left-slider-01">
-            <h2>بدون قانون ریسک شناور و mdl</h2>
-            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است،  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها</p>
-            <div class="btn-sec-slider-01">
-                <button class="slider-login-btn">ورود / عضویت</button>
-                <button class="slider-preview-dashboard-btn">پیش نمایش داشبورد</button>
+            <button onclick="window.location.href='https://nitroprop.live/panel/" class="slider-login-btn">خرید چالش</button>
+            <button class="slider-preview-dashboard-btn">ورود به داشبورد</button>
             </div>
         </div>
         <div class="right-slider-01">
@@ -42,14 +29,29 @@
     <div class="mySlides fade slider-01" style="display: none">
         <div class="left-slider-01">
             <h2>معاملات سواپ فری در سرور اختصاصی نیتروپراپ</h2>
-            <p>تمامی حساب ها در سرور اختصاصی نیتروپراپ سواپ فری هستند و می توانید روز ها و هفته ها معاملات خود را باز نگه دارید تا به سود موردنظر خود برسید.</p>
+            <p>
+            تمامی حساب ها در سرور اختصاصی نیتروپراپ سواپ فری هستند و می توانید روز ها و هفته ها معاملات خود را باز نگه دارید تا به سود موردنظر خود برسید.
+            </p>
             <div class="btn-sec-slider-01">
-                <button class="slider-login-btn">ورود / عضویت</button>
-                <button class="slider-preview-dashboard-btn">پیش نمایش داشبورد</button>
+            <button onclick="window.location.href='https://nitroprop.live/panel/" class="slider-login-btn">خرید چالش</button>
+            <button class="slider-preview-dashboard-btn">ورود به داشبورد</button>
             </div>
         </div>
         <div class="right-slider-01">
-            <img src="<?php echo NCP_PLUGIN_INCLUDES_URL . 'front-assets/img/slider/slider-02.png' ?>" alt="">
+            <img src="<?php echo NCP_PLUGIN_INCLUDES_URL . 'front-assets/img/slider/slider-03.png' ?>" alt="">
+        </div>
+    </div>
+    <div class="mySlides fade slider-01" style="display: none">
+        <div class="left-slider-01">
+            <h2>با نیتروپراپ بدون محدودیت زمانی معامله کنید!</h2>
+            <p>در نیتروپراپ چالش ها محدودیت زمانی ندارند و بدون نگرانی از بابت محدودیت های ۳۰ و ۶۰ روزه برای پاس کردن چالش ها ، می توانید مراحل چالش خود را طی کنید.</p>
+            <div class="btn-sec-slider-01">
+            <button onclick="window.location.href='https://nitroprop.live/panel/" class="slider-login-btn">خرید چالش</button>
+            <button class="slider-preview-dashboard-btn">ورود به داشبورد</button>
+            </div>
+        </div>
+        <div class="right-slider-01">
+            <img src="<?php echo NCP_PLUGIN_INCLUDES_URL . 'front-assets/img/slider/slider-04.png' ?>" alt="">
         </div>
     </div>
 </div>
